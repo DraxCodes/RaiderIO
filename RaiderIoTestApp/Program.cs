87197da6143048db07d5ee8c41a93be3f791e46c
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-29 14:53:51 +0000

    Add Test App

---
+++ RaiderIoTestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using RaiderIO;
+using RaiderIO.Entities.Enums;
+using System;
+
+namespace RaiderIoTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RaiderIOClient(Region.EU, "Draenor", "Shamkie");
+            Console.WriteLine($"Extended Character Test: {client.Champion.GetRaidProgression.Uldir.Summary}");
+
+            var mythicplus = client.GetMythicPlusData();
+            foreach (var item in mythicplus.RecentRuns)
+            {
+                Console.WriteLine($"Mythic Plus Test: {item.DungeonName}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
