d3f579a5e002723488140bb0028f19e9c952ed65
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:50:08 +0000

    Added tests for all added Methods.

--- RaiderIoTestApp/Program.cs
+++ RaiderIoTestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RaiderIO;
 using RaiderIO.Entities.Enums;
+using RaiderIO.Entities.MythicPlusRuns;
 using System;
 
 namespace RaiderIoTestApp
@@ -8,20 +9,40 @@ namespace RaiderIoTestApp
     {
         static void Main(string[] args)
         {
-            var client = new RaiderIOClient(Region.EU, "Draenor", "Shamkie");
+            var client = new RaiderIOClient(Region.EU, "Draenor", "Perifete");
             Console.WriteLine($"Extended Character Test: {client.Champion.GetRaidProgression.Uldir.Summary}");
 
-            var mythicplus = client.GetMythicPlusData(3);
-            foreach (var item in mythicplus.RecentRuns)
+            var recent = client.GetRecentRuns();
+            Test(recent.RecentRuns, "Recent Runs");
+
+            var best = client.GetBestRuns(3);
+            Test(best.BestRuns, "Best Runs");
+
+            var weekly = client.GetWeeklyRuns();
+            Test(weekly.WeeklyRuns, "Weekly Runs");
+
+            var highest = client.GetHighestRuns();
+            Test(highest.HighestRuns, "Highest Runs");
+
+            var rankings = client.GetMythicPlusRankings();
+            Console.WriteLine($"\n\n RANKS\nOverall: {rankings.Rankings.Overall.World}\nRealm DPS: {rankings.Rankings.Dps.Realm}");
+
+            var affixes = client.GetAffixes(Region.EU);
+            Console.WriteLine("\n\nAffixes");
+            foreach (var item in affixes.CurrentAffixes)
             {
-                Console.WriteLine($"Mythic Plus Test[Recent Runs]: {item.DungeonName} - LevelUp: {item.KeystoneUpgradeNum}");
+                Console.WriteLine($"{item.Name}\n{item.Description}\n");
             }
 
-            foreach (var item in mythicplus.BestRuns)
-            {
-                Console.WriteLine($"Mythic Plus Test[Best Runs]: {item.DungeonName} - LevelUp: {item.KeystoneUpgradeNum}");
-            }
             Console.ReadLine();
+        }
+
+        public static void Test(BaseRuns[] runs, string type)
+        {
+            foreach (var item in runs)
+            {
+                Console.WriteLine($"Mythic Plus Test[{type}]: {item.DungeonName} [Level: {item.Level}] - LevelUp: {item.KeystoneUpgradeNum}");
+            }
         }
     }
 }
