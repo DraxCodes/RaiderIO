97fd26b08d20f7294bb99249294858ef4c245b9c
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:48:50 +0000

    Added Best Runs Entity

---
+++ RaiderIO/Entities/MythicPlusRuns/MpBestRuns.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace RaiderIO.Entities.MythicPlusRuns
+{
+    public class MpBestRuns
+    {
+        [JsonProperty("mythic_plus_best_runs")]
+        public BaseRuns[] BestRuns { get; set; }
+    }
+}
