559c89aadfe26fb35c3799337b66534d9aa4ee72
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:49:09 +0000

    Added Rankings Entity

---
+++ RaiderIO/Entities/MythicPlusRuns/MpRanking.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+
+namespace RaiderIO.Entities.MythicPlusRuns
+{
+
+    public class MpRanking
+    {
+        [JsonProperty("mythic_plus_ranks")]
+        public Ranks Rankings { get; set; }
+    }
+
+    public class Ranks
+    {
+        [JsonProperty("overall")]
+        public Areas Overall { get; set; }
+
+        [JsonProperty("dps")]
+        public Areas Dps { get; set; }
+
+        [JsonProperty("healer")]
+        public Areas Healer { get; set; }
+
+        [JsonProperty("tank")]
+        public Areas Tank { get; set; }
+
+        [JsonProperty("class")]
+        public Areas ClassOverall { get; set; }
+
+        [JsonProperty("class_dps")]
+        public Areas ClassDPS { get; set; }
+
+        [JsonProperty("class_healer")]
+        public Areas ClassHealer { get; set; }
+
+        [JsonProperty("class_tank")]
+        public Areas ClassTank { get; set; }
+    }
+
+    public class Areas
+    {
+        [JsonProperty("world")]
+        public int World { get; set; }
+
+        [JsonProperty("region")]
+        public int Region { get; set; }
+
+        [JsonProperty("realm")]
+        public int Realm { get; set; }
+    }
+}
