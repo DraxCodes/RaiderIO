09cb4b6103c0bbe4e9af75b33df1e3a6f6f89b5e
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:49:00 +0000

    Added Highest Runs Entity

---
+++ RaiderIO/Entities/MythicPlusRuns/MpHighestRuns.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace RaiderIO.Entities.MythicPlusRuns
+{
+    public class MpHighestRuns
+    {
+        [JsonProperty("mythic_plus_highest_level_runs")]
+        public BaseRuns[] HighestRuns { get; set; }
+    }
+}
