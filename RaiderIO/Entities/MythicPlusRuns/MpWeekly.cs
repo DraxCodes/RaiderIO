276f4a50b3c77d82a08649aa886d5cba5204d251
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:49:39 +0000

    Added Weekly Entity

---
+++ RaiderIO/Entities/MythicPlusRuns/MpWeekly.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace RaiderIO.Entities.MythicPlusRuns
+{
+    public class MpWeeklyRuns
+    {
+        [JsonProperty("mythic_plus_weekly_highest_level_runs")]
+        public BaseRuns[] WeeklyRuns { get; set; }
+    }
+}
