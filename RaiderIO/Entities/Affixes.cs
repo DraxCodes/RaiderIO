82e98a102a6ef9322b24d8046ad010660a2ddde4
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:48:40 +0000

    Added Affix Entity

---
+++ RaiderIO/Entities/Affixes.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace RaiderIO.Entities
+{
+    public class Affixes
+    {
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("leaderboard_url")]
+        public string LeaderboardUrl { get; set; }
+
+        [JsonProperty("affix_details")]
+        public CurrentAffixes[] CurrentAffixes { get; set; }
+    }
+
+    public class CurrentAffixes
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("wowhead_url")]
+        public string Url { get; set; }
+    }
+
+}
