70d473774b340e982ffe9ba829ef6ad4a5371ace
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:49:57 +0000

    Added Get Methods For all added Enitities.

--- RaiderIO/RaiderIOClient.cs
+++ RaiderIO/RaiderIOClient.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using RaiderIO.Entities;
 using RaiderIO.Entities.Enums;
+using RaiderIO.Entities.MythicPlusRuns;
 using System;
 using System.Net;
 
@@ -28,12 +29,80 @@ namespace RaiderIO
         /// <summary>
         ///     Gets The Mythic+ Data For The Character.
         /// </summary>
-        /// <param name="count">The Amount of Best Runs You wish to Return. (Recent runs always returns 3)</param>
         /// <returns></returns>
-        public MythicPlus GetMythicPlusData(int count)
+        public MpRecentRuns GetRecentRuns()
         {
-            var baseUrl = $"{GetBaseUrl(Region)}&realm={Realm}&name={Name}&fields=mythic_plus_best_runs:{count}%2Cmythic_plus_recent_runs";
-            return DeserializeJson(DataType.MythicPlus, baseUrl) as MythicPlus;
+            var baseUrl = $"{GetBaseUrl(Region)}&realm={Realm}&name={Name}&fields=mythic_plus_recent_runs";
+            return DeserializeJson(DataType.MythicPlusRecent, baseUrl) as MpRecentRuns;
+        }
+
+        /// <summary>
+        ///  Gets the Mythic+ Best Runs for the Character
+        /// </summary>
+        /// <param name="count">The Number of results to return.</param>
+        /// <returns></returns>
+        public MpBestRuns GetBestRuns(int count)
+        {
+            var baseUrl = $"{GetBaseUrl(Region)}&realm={Realm}&name={Name}&fields=mythic_plus_best_runs:{count}";
+            return DeserializeJson(DataType.MythicPlusBest, baseUrl) as MpBestRuns;
+        }
+
+        /// <summary>
+        /// Gets the Mythic+ Weekly Runs for the Character.
+        /// </summary>
+        /// <returns></returns>
+        public MpWeeklyRuns GetWeeklyRuns()
+        {
+            var baseUrl = $"{GetBaseUrl(Region)}&realm={Realm}&name={Name}&fields=mythic_plus_weekly_highest_level_runs";
+            return DeserializeJson(DataType.MythicPlusWeekly, baseUrl) as MpWeeklyRuns;
+        }
+
+        /// <summary>
+        /// Gets The Mythic+ Highest Runs for the Character.
+        /// </summary>
+        /// <returns></returns>
+        public MpHighestRuns GetHighestRuns()
+        {
+            var baseUrl = $"{GetBaseUrl(Region)}&realm={Realm}&name={Name}&fields=mythic_plus_highest_level_runs";
+            return DeserializeJson(DataType.MythicPlusHighest, baseUrl) as MpHighestRuns;
+        }
+
+        /// <summary>
+        /// Gets the Mythic+ Rankings for the Chartacter.
+        /// </summary>
+        /// <returns></returns>
+        public MpRanking GetMythicPlusRankings()
+        {
+            var baseUrl = $"{GetBaseUrl(Region)}&realm={Realm}&name={Name}&fields=mythic_plus_ranks";
+            return DeserializeJson(DataType.MythicPlusRanking, baseUrl) as MpRanking;
+        }
+
+        /// <summary>
+        /// Gets The current weeks Mythic+ Affixes.
+        /// </summary>
+        /// <param name="region">The Region you want to retrieve Affixes for.</param>
+        /// <returns></returns>
+        public Affixes GetAffixes(Region region)
+        {
+            string baseUrl = String.Empty;
+            switch (region)
+            {
+                case Region.US:
+                    baseUrl = "https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en";
+                    break;
+                case Region.EU:
+                    baseUrl = "https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=en";
+                    break;
+                case Region.KR:
+                    baseUrl = "https://raider.io/api/v1/mythic-plus/affixes?region=kr&locale=en";
+                    break;
+                case Region.TW:
+                    baseUrl = "https://raider.io/api/v1/mythic-plus/affixes?region=tw&locale=en";
+                    break;
+                default:
+                    break;
+            }
+            return DeserializeJson(DataType.MythicPlusAffixes, baseUrl) as Affixes;
         }
 
         private object DeserializeJson(DataType type, string baseUrl)
@@ -42,8 +111,18 @@ namespace RaiderIO
             {
                 case DataType.Character:
                     return JsonConvert.DeserializeObject<CharacterExtended>(GetRawData(baseUrl));
-                case DataType.MythicPlus:
-                    return JsonConvert.DeserializeObject<MythicPlus>(GetRawData(baseUrl));
+                case DataType.MythicPlusRecent:
+                    return JsonConvert.DeserializeObject<MpRecentRuns>(GetRawData(baseUrl));
+                case DataType.MythicPlusBest:
+                    return JsonConvert.DeserializeObject<MpBestRuns>(GetRawData(baseUrl));
+                case DataType.MythicPlusWeekly:
+                    return JsonConvert.DeserializeObject<MpWeeklyRuns>(GetRawData(baseUrl));
+                case DataType.MythicPlusHighest:
+                    return JsonConvert.DeserializeObject<MpHighestRuns>(GetRawData(baseUrl));
+                case DataType.MythicPlusRanking:
+                    return JsonConvert.DeserializeObject<MpRanking>(GetRawData(baseUrl));
+                case DataType.MythicPlusAffixes:
+                    return JsonConvert.DeserializeObject<Affixes>(GetRawData(baseUrl));
                 default:
                     throw new NotSupportedException("The Requested Option Is Not Supported.");
             }
