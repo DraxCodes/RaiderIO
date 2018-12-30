23a51149a1d099a9b9edefeea0eade12836e1c2a
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:48:29 +0000

    Added Base Runs Entity

---
+++ RaiderIO/Entities/MythicPlusRuns/BaseRuns.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RaiderIO.Entities.MythicPlusRuns
+{
+    public class BaseRuns
+    {
+        [JsonProperty("dungeon")]
+        public string DungeonName { get; set; }
+
+        [JsonProperty("short_name")]
+        public string DungeonShortName { get; set; }
+
+        [JsonProperty("mythic_level")]
+        public int Level { get; set; }
+
+        [JsonProperty("completed_at")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("clear_time_ms")]
+        public int ClearTime { get; set; }
+
+        [JsonProperty("num_keystone_upgrades")]
+        public int KeystoneUpgradeNum { get; set; }
+
+        [JsonProperty("map_challenge_mode_id")]
+        public int Id { get; set; }
+
+        [JsonProperty("score")]
+        public float Score { get; set; }
+
+        [JsonProperty("affixes")]
+        public Affix[] Affixes { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+    public class Affix
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("wowhead_url")]
+        public string URL { get; set; }
+    }
+
+}
