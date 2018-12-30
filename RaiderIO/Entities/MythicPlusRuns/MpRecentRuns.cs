852bb7a20cf56b33238bb8b2f374df69066eaedc
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-30 13:49:22 +0000

    Added Recent Runs Entity

---
+++ RaiderIO/Entities/MythicPlusRuns/MpRecentRuns.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace RaiderIO.Entities.MythicPlusRuns
+{
+    public class MpRecentRuns
+    {
+        [JsonProperty("mythic_plus_recent_runs")]
+        public BaseRuns[] RecentRuns { get; set; }
+    }
+}
