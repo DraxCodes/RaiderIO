63e9ae3099e340f57c0d838e97d72ba7547fc48a
Joel Parkinson <joelparkinson@live.co.uk>
2019-01-01 21:10:21 +0000

    Update Program.cs

--- RaiderIoTestApp/Program.cs
+++ RaiderIoTestApp/Program.cs
@@ -13,38 +13,45 @@ namespace RaiderIoTestApp
 
         private static async Task Initialize()
         {
+            //The Params Defined Here will be used for all of the core Character Specific Requests.
             var client = new RaiderIOClient(Region.EU, "Draenor", "Perifete");
             var characterStats = await client.GetCharacterStats();
             Console.WriteLine($"Extended Character Test: {characterStats.GetRaidProgression.Uldir.Summary}");
 
-            //var recent = await client.GetRecentRuns();
-            //Test(recent.RecentRuns, "Recent Runs");
+            var recent = await client.GetRecentRuns();
+            Test(recent.RecentRuns, "Recent Runs");
 
-            //var best = await client.GetBestRuns(3);
-            //Test(best.BestRuns, "Best Runs");
+            var best = await client.GetBestRuns(3);
+            Test(best.BestRuns, "Best Runs");
 
-            //var weekly = await client.GetWeeklyRuns();
-            //Test(weekly.WeeklyRuns, "Weekly Runs");
+            var weekly = await client.GetWeeklyRuns();
+            Test(weekly.WeeklyRuns, "Weekly Runs");
 
-            //var highest = await client.GetHighestRuns();
-            //Test(highest.HighestRuns, "Highest Runs");
+            var highest = await client.GetHighestRuns();
+            Test(highest.HighestRuns, "Highest Runs");
 
-            //var rankings = await client.GetMythicPlusRankings();
-            //Console.WriteLine($"\n\n RANKS\nOverall: {rankings.Rankings.Overall.World}\nRealm DPS: {rankings.Rankings.Dps.Realm}");
+            var rankings = await client.GetMythicPlusRankings();
+            Console.WriteLine($"\n\n RANKS\nOverall: {rankings.Rankings.Overall.World}\nRealm DPS: {rankings.Rankings.Dps.Realm}");
 
-            //var affixes = await client.GetAffixes(Region.EU);
-            //Console.WriteLine("\n\nAffixes");
-            //foreach (var item in affixes.CurrentAffixes)
-            //{
-            //    Console.WriteLine($"{item.Name}\n{item.Description}\n");
-            //}
+            //This requires the Region param again to allow for the user to request affixes for any region.
+            //regardless of those defined in the RaiderIOClient. 
+            var affixes = await client.GetAffixes(Region.EU);
+            Console.WriteLine("\n\nAffixes");
+            foreach (var item in affixes.CurrentAffixes)
+            {
+                Console.WriteLine($"{item.Name}\n{item.Description}\n");
+            }
 
-            //var guildprog = await client.GetGuildRaidProgression(Region.EU, "draenor", "Phoenix Arising");
-            //Console.WriteLine($"\n\nGuild Raid Summary: {guildprog.RaidInfo.Uldir.Summary}");
+            //This requires additonal params due to not actually calling character specific data.
+            //Instead here, we're calling guild data.
+            var guildprog = await client.GetGuildRaidProgression(Region.EU, "draenor", "Phoenix Arising");
+            Console.WriteLine($"\n\nGuild Raid Summary: {guildprog.RaidInfo.Uldir.Summary}");
 
             Console.ReadLine();
         }
 
+        //Simple test method to run tests faster.
+        //TODO: Implement Unit Tests.
         public static void Test(BaseRuns[] runs, string type)
         {
             foreach (var item in runs)
